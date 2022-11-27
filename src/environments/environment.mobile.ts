bf0a8b0fdcc8528cdc940d9602b8d875e097a506
Mike Static <mike.stotik@alseda.by>
2022-11-27 11:36:52 +0300

    added environments

---
+++ src/environments/environment.mobile.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  logs: {
+    ngxs: false
+  },
+  api: {
+    core: `https://yourday.me/api/v1.0`
+  },
+  websocket: {
+    url: 'https://yourday.me/'
+  }
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  logs: {
+    ngxs: false
+  },
+  api: {
+    core: `/api/v1.0`
+  },
+  websocket: {
+    url: '/'
+  }
+};
