7ea9d2de2ce795c36bae3862b71c46adcf94ac27
Mike Static <mike.stotik@alseda.by>
2022-11-30 08:50:13 +0300

    fixed styles

--- src/app/config/hammer.config.ts
+++ src/app/config/hammer.config.ts
@@ -5,11 +5,26 @@ import * as Hammer from 'hammerjs';
 
 @Injectable()
 export class HammerConfig extends HammerGestureConfig {
-  override overrides = {
-    swipe: {
-      direction: Hammer.DIRECTION_ALL
-    },
-    pinch: { enable: false },
-    rotate: { enable: false }
-  };
+
+  override buildHammer(element: HTMLElement) {
+    const options: HammerOptions = {}
+
+    const mc = new Hammer(element, options);
+
+    mc.get('swipe').set({ enable: true, direction: Hammer.DIRECTION_ALL });
+
+    // if (element.classList.contains('bottom-sheet')) {
+    //   mc.get('swipe').set({ enable: true, direction: Hammer.DIRECTION_DOWN });
+    // } else {
+    //   mc.get('swipe').set({ enable: false, direction: Hammer.DIRECTION_DOWN });
+    // }
+    //
+    // if (element.classList.contains('task-list')) {
+    //   mc.get('swipe').set({ enable: false });
+    // } else {
+    //   mc.get('swipe').set({ enable: true });
+    // }
+
+    return mc;
+  }
 }
