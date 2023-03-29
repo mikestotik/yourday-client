9ea7545c0a5a4597866fc9cbb342adc4fcaab6b9
Mike Static <mike.stotik@alseda.by>
2023-03-29 09:37:04 +0300

    added update pass form

--- src/app/views/auth/auth.component.ts
+++ src/app/views/auth/auth.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { AuthState } from '../../models/auth/store/auth.state';
 
 
 @Component({
   templateUrl: './auth.component.html',
   styleUrls: [ './auth.component.scss' ]
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
 
+  constructor(
+    private readonly store: Store) {
+  }
+
+
+  public ngOnInit(): void {
+    this.store.reset(AuthState);
+  }
 }
